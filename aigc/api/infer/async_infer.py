8367cc3dd6957282a8cc57fc063d5178bb84bf04
Zeng Yu <zeng.yu.1993@gmail.com>
2025-07-02 16:46:19 +0800

    upd: implement async infer APIs,

--- aigc/api/infer/async_infer.py
+++ aigc/api/infer/async_infer.py
@@ -1,83 +1,263 @@
-from fastapi import APIRouter, Depends, Response, Request
-from fastapi.routing import APIRoute
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Depends, Response, Request, BackgroundTasks
 from functools import cache
-from typing import Annotated, Any, Callable, Coroutine
+from typing import Annotated, Mapping
 
-from ... import background_inference as bgi
-from ... import common
 from ... import deps
 from pydantic import BaseModel
+import httpx
+from .common import point_manager, NoPointError, InferResponse, InferRoute, NotDownError
+import asyncio
+from dataclasses import dataclass, field
+import secrets
+from loguru import logger
 
-
-# Exception raise when try get response no wait but request still in progress.
-class NotDownError(Exception):
-
-    def __init__(self, tid: str) -> None:
-        self.tid: str = tid
-
-    def __str__(self) -> str:
-        return f"request {self.tid} still in progress."
-
-
-# Custom route class in order to handle exceptions.
-class AsyncInferRoute(APIRoute):
-
-    def get_route_handler(self) -> Callable[[Request], Coroutine[Any, Any, Response]]:
-        origional_handler = super().get_route_handler()
-
-        async def custom_handler(req: Request) -> Response:
-            try:
-                return await origional_handler(req)
-
-            except NotDownError as exc:
-                return JSONResponse(content={"code": 10, "msg": str(exc)})
-
-            except common.excpetions.NotFoundError:
-                return JSONResponse(
-                    content={"code": 10, "msg": "no such request, check tid."}
-                )
-
-        return custom_handler
-
-
-# Get background request dict with cache
-# It always call with no arguments so it always return the same object.
-# Like a singleton.
-@cache
-def get_background_request_dict() -> bgi.ReqDict:
-    return bgi.ReqDict()
-
-
-# Depend use to manage background requests.
-BackgroundRequests = Annotated[bgi.ReqDict, Depends(get_background_request_dict)]
+STATE_IN_PROGRESS: str = "in progress"
+STATE_DOWN: str = "down"
 
 
 # Response model when append new request and query requests state.
-class RequestState(BaseModel):
+class GetStateResponse(BaseModel):
+    code: int
+    msg: str
     tid: str
-    state: bgi.State
+    state: str
+
+
+# Response model when create new background task.
+class CreateRequestResponse(BaseModel):
+    code: int
+    msg: str
+    tid: str
+
+
+# Define background request metadata.
+@dataclass
+class BackgroundRequest:
+    uid: int
+    point: int
+    content: bytes
+    headers: Mapping[str, str]
+    tid: str = field(default_factory=lambda: secrets.token_hex(8))
+    exception: Exception | None = field(default=None, init=False)
+    response: Response | None = field(default=None, init=False)
+    cond: asyncio.Condition = field(default_factory=asyncio.Condition, init=False)
+
+
+# Class use to manage background requests.
+class BackgroundRequestsDict:
+
+    def __init__(self) -> None:
+        self.lock = asyncio.Lock()
+        self.responses_by_uid: dict[int, dict[str, BackgroundRequest]] = {}
+
+
+# A function to build requests dict with cache.
+# Due to the arguments always the same so it always return same object.
+@cache
+def get_requests_dict() -> BackgroundRequestsDict:
+    return BackgroundRequestsDict()
+
+
+# Depends of requests dict.
+RequestsDict = Annotated[BackgroundRequestsDict, Depends(get_requests_dict)]
+
+
+# Forward request to infer server, and set raw response when complete.
+async def forward_to_infer(
+    url: str,
+    req: BackgroundRequest,
+    db: deps.Database,
+):
+    async with httpx.AsyncClient(timeout=None) as client:
+        resp = await client.post(url, content=req.content, headers=req.headers)
+        logger.debug(f"infer server response request {req.tid}")
+
+    try:
+        resp = resp.raise_for_status()
+
+        async with point_manager(req.uid, db) as pm:
+            infer_response = InferResponse.model_validate_json(resp.content)
+            if infer_response.code != 0:
+                pm.recharge(req.point)
+                logger.info(f"background infer {req.tid} error, recharge point.")
+            logger.info(f"background infer request {req.tid} complete.")
+
+            response = Response(content=resp.content, headers=resp.headers)
+            async with req.cond:
+                req.response = response
+                req.cond.notify_all()
+
+    except Exception as exc:
+        async with req.cond:
+            req.exception = exc
+            req.cond.notify_all()
 
 
 # Define router
-router = APIRouter(prefix="/async/infer", route_class=AsyncInferRoute)
+router = APIRouter(prefix="/async/infer", route_class=InferRoute)
 
 
 # API to query background requests current state, no block.
 @router.get("/{tid}/state")
 async def get_req_state(
-    tid: str, ses: deps.UserSession, requests: BackgroundRequests
-) -> RequestState:
-    state = await requests.state(tid)
-    return RequestState(tid=tid, state=state)
+    tid: str, ses: deps.UserSession, req_dict: RequestsDict
+) -> GetStateResponse:
+
+    response = GetStateResponse(code=0, msg="ok", tid=tid, state=STATE_IN_PROGRESS)
+
+    async with req_dict.lock:
+        infer_request = req_dict.responses_by_uid[ses.uid][tid]
+        if infer_request.exception or infer_request.response:
+            response.state = STATE_DOWN
+
+    return response
 
 
 # API to get result of a request if have, no block.
 @router.get("/{tid}/result")
 async def get_req_result(
-    tid: str, ses: deps.UserSession, requests: BackgroundRequests
+    tid: str, ses: deps.UserSession, req_dict: RequestsDict
 ) -> Response:
-    res = await requests.response(tid)
-    if res is None:
+
+    async with req_dict.lock:
+        infer_request = req_dict.responses_by_uid[ses.uid][tid]
+
+        if infer_request.exception:
+            raise infer_request.exception
+
+        if infer_request.response:
+            return infer_request.response
+
         raise NotDownError(tid)
-    return Response(content=res.content, headers=res.headers)
+
+
+# API to create a background replace with any infer request.
+@router.post("/replace_any")
+async def replace_with_any(
+    req: Request,
+    ses: deps.UserSession,
+    db: deps.Database,
+    conf: deps.Config,
+    req_dict: RequestsDict,
+    bg: BackgroundTasks,
+) -> CreateRequestResponse:
+    async with point_manager(ses.uid, db) as pm:
+        if pm.magic_points < 10:
+            raise NoPointError(ses.uid)
+
+    # Gen task id, create memories.
+    content = await req.body()
+    headers = req.headers
+    bg_req = BackgroundRequest(ses.uid, 10, content, headers)
+
+    async with req_dict.lock:
+        if ses.uid not in req_dict.responses_by_uid:
+            req_dict.responses_by_uid[ses.uid] = {}
+        req_dict.responses_by_uid[ses.uid][bg_req.tid] = bg_req
+
+    # Add background task.
+    url = conf.infer.base + conf.infer.replace_any
+    bg.add_task(forward_to_infer, url, bg_req, db)
+
+    # Pre deduct point prevent excceed limit.
+    pm.deduct(10)
+    return CreateRequestResponse(code=0, msg="ok", tid=bg_req.tid)
+
+
+# API to create a background replace with reference infer request.
+@router.post("/replace_with_reference")
+async def replace_with_reference(
+    req: Request,
+    ses: deps.UserSession,
+    db: deps.Database,
+    conf: deps.Config,
+    req_dict: RequestsDict,
+    bg: BackgroundTasks,
+) -> CreateRequestResponse:
+    async with point_manager(ses.uid, db) as pm:
+        if pm.magic_points < 10:
+            raise NoPointError(ses.uid)
+
+    # Gen task id, create memories.
+    content = await req.body()
+    headers = req.headers
+    bg_req = BackgroundRequest(ses.uid, 10, content, headers)
+
+    async with req_dict.lock:
+        if ses.uid not in req_dict.responses_by_uid:
+            req_dict.responses_by_uid[ses.uid] = {}
+        req_dict.responses_by_uid[ses.uid][bg_req.tid] = bg_req
+
+    # Add background task.
+    url = conf.infer.base + conf.infer.replace_reference
+    bg.add_task(forward_to_infer, url, bg_req, db)
+
+    # Pre deduct point prevent excceed limit.
+    pm.deduct(10)
+    return CreateRequestResponse(code=0, msg="ok", tid=bg_req.tid)
+
+
+# API to create a background image to video request.
+@router.post("/image_to_video")
+async def image_to_video(
+    req: Request,
+    ses: deps.UserSession,
+    db: deps.Database,
+    conf: deps.Config,
+    req_dict: RequestsDict,
+    bg: BackgroundTasks,
+) -> CreateRequestResponse:
+    async with point_manager(ses.uid, db) as pm:
+        if pm.magic_points < 10:
+            raise NoPointError(ses.uid)
+
+    # Gen task id, create memories.
+    content = await req.body()
+    headers = req.headers
+    bg_req = BackgroundRequest(ses.uid, 10, content, headers)
+
+    async with req_dict.lock:
+        if ses.uid not in req_dict.responses_by_uid:
+            req_dict.responses_by_uid[ses.uid] = {}
+        req_dict.responses_by_uid[ses.uid][bg_req.tid] = bg_req
+
+    # Add background task.
+    url = conf.infer.base + conf.infer.image_to_video
+    bg.add_task(forward_to_infer, url, bg_req, db)
+
+    # Pre deduct point prevent excceed limit.
+    pm.deduct(10)
+    return CreateRequestResponse(code=0, msg="ok", tid=bg_req.tid)
+
+
+# API to create a background segment any infer request.
+@router.post("/segment_any")
+async def segment_any(
+    req: Request,
+    ses: deps.UserSession,
+    db: deps.Database,
+    conf: deps.Config,
+    req_dict: RequestsDict,
+    bg: BackgroundTasks,
+) -> CreateRequestResponse:
+    async with point_manager(ses.uid, db) as pm:
+        if pm.magic_points < 10:
+            raise NoPointError(ses.uid)
+
+    # Gen task id, create memories.
+    content = await req.body()
+    headers = req.headers
+    bg_req = BackgroundRequest(ses.uid, 10, content, headers)
+
+    async with req_dict.lock:
+        if ses.uid not in req_dict.responses_by_uid:
+            req_dict.responses_by_uid[ses.uid] = {}
+        req_dict.responses_by_uid[ses.uid][bg_req.tid] = bg_req
+
+    # Add background task.
+    url = conf.infer.base + conf.infer.segment_any
+    bg.add_task(forward_to_infer, url, bg_req, db)
+
+    # Pre deduct point prevent excceed limit.
+    pm.deduct(10)
+    return CreateRequestResponse(code=0, msg="ok", tid=bg_req.tid)
--- aigc/api/infer/sync_infer.py
+++ aigc/api/infer/sync_infer.py
@@ -1,102 +1,13 @@
-from collections.abc import AsyncIterator
-from contextlib import asynccontextmanager
-from datetime import datetime
-from typing import Any, Callable, Coroutine
-
 import httpx
 from fastapi import APIRouter, Request, Response
-from fastapi.responses import JSONResponse
-from fastapi.routing import APIRoute
 from loguru import logger
-from pydantic import BaseModel, ValidationError
-from sqlmodel import Session, select
 
 from ... import deps
-from ...models.db import MagicPointSubscription, SubscriptionType
+
+from .common import NoPointError, InferResponse, point_manager, InferRoute
 
 
-class InferRoute(APIRoute):
-
-    def get_route_handler(self) -> Callable[[Request], Coroutine[Any, Any, Response]]:
-        original_route_handler = super().get_route_handler()
-
-        async def route_handler(req: Request) -> Response:
-            try:
-                return await original_route_handler(req)
-
-            # HTTPStatusError will raise when forward to infer srver.
-            except httpx.HTTPStatusError as exc:
-                logger.error(f"infer server unavailable, {repr(exc)}")
-                return JSONResponse(
-                    content={"code": 1, "msg": "infer server unavailable"}
-                )
-
-            # ValidationError will raise when try parse infer server response body to a json.
-            except ValidationError as exc:
-                logger.error(f"infer response invalid, {repr(exc)}")
-                return JSONResponse(
-                    content={"code": 2, "msg": "infer response invalid"}
-                )
-
-            # NoPointError will raise when user do not have enough point but try infer some.
-            except NoPointError as exc:
-                logger.info(f"user {exc.uid} do not have magic point")
-                return JSONResponse(
-                    content={"code": 3, "msg": "magic point not enough"}
-                )
-
-        return route_handler
-
-
-router = APIRouter(prefix="/infer", route_class=InferRoute)
-
-
-class NoPointError(Exception):
-    def __init__(self, uid: int) -> None:
-        self.uid = uid
-
-
-class InferResponse(BaseModel):
-    code: int
-
-
-@asynccontextmanager
-async def query_valid_subscription(
-    uid: int, db: Session
-) -> AsyncIterator[MagicPointSubscription]:
-    query = (
-        select(MagicPointSubscription)
-        .where(MagicPointSubscription.uid == uid)
-        .where(MagicPointSubscription.expired == False)
-    )
-    subscriptions = db.exec(query).all()
-
-    trail: list[MagicPointSubscription] = []
-    payed: list[MagicPointSubscription] = []
-
-    for s in subscriptions:
-        if s.stype == SubscriptionType.trail:
-            trail.append(s)
-        if s.stype == SubscriptionType.subscription:
-            payed.append(s)
-
-    subscription: MagicPointSubscription | None = None
-    if len(payed) != 0:
-        subscription = payed[0]
-    elif len(trail) != 0:
-        subscription = trail[0]
-    else:
-        raise AssertionError("no valid subscriptions")
-
-    if subscription.remains <= 0:
-        raise NoPointError(uid)
-
-    try:
-        yield subscription
-    finally:
-        db.commit()
-
-
+# Forward request to infer server and return is resp code and body.
 async def forward_to_infer_srv(url: str, req: Request) -> tuple[int, Response]:
     async with httpx.AsyncClient(timeout=None) as client:
         content = await req.body()
@@ -108,6 +19,9 @@ async def forward_to_infer_srv(url: str, req: Request) -> tuple[int, Response]:
         return infer_resp.code, Response(content=resp.content, headers=resp.headers)
 
 
+router = APIRouter(prefix="/infer", route_class=InferRoute)
+
+
 @router.post("/image")
 async def old_replace_with_reference(
     req: Request,
@@ -116,15 +30,19 @@ async def old_replace_with_reference(
     conf: deps.Config,
 ) -> Response:
 
-    async with query_valid_subscription(ses.uid, db) as subscription:
+    async with point_manager(ses.uid, db) as pm:
+        if pm.magic_points < 10:
+            raise NoPointError(ses.uid)
+
         url = conf.infer.base + conf.infer.replace_any
         code, resp = await forward_to_infer_srv(url, req)
+
         if code == 0:
-            subscription.remains -= 10
-            subscription.utime = datetime.now()
+            pm.deduct(10)
             logger.info(f"reduce user {ses.nickname} magic points.")
 
         return resp
+
 
 @router.post("/replace_any")
 async def replace_with_any(
@@ -134,16 +52,20 @@ async def replace_with_any(
     conf: deps.Config,
 ) -> Response:
 
-    async with query_valid_subscription(ses.uid, db) as subscription:
-        url = conf.infer.base + conf.infer.replace_reference
+    async with point_manager(ses.uid, db) as pm:
+        if pm.magic_points < 10:
+            raise NoPointError(ses.uid)
+
+        url = conf.infer.base + conf.infer.replace_any
         code, resp = await forward_to_infer_srv(url, req)
+
         if code == 0:
-            subscription.remains -= 10
-            subscription.utime = datetime.now()
+            pm.deduct(10)
             logger.info(f"reduce user {ses.nickname} magic points.")
 
         return resp
-    
+
+
 @router.post("/replace_with_reference")
 async def replace_with_reference(
     req: Request,
@@ -152,13 +74,16 @@ async def replace_with_reference(
     conf: deps.Config,
 ) -> Response:
 
-    async with query_valid_subscription(ses.uid, db) as subscription:
+    async with point_manager(ses.uid, db) as pm:
+        if pm.magic_points < 10:
+            raise NoPointError(ses.uid)
+
         url = conf.infer.base + conf.infer.replace_any
         code, resp = await forward_to_infer_srv(url, req)
+
         if code == 0:
-            subscription.remains -= 10
-            subscription.utime = datetime.now()
-            logger.info(f"reduce user {ses.nickname} magic points.")
+            pm.deduct(10)
+            logger.info(f"deduct user {ses.nickname} magic points.")
 
         return resp
 
@@ -168,12 +93,15 @@ async def make_i2v_process(
     req: Request, ses: deps.UserSession, db: deps.Database, conf: deps.Config
 ) -> Response:
 
-    async with query_valid_subscription(ses.uid, db) as subscription:
+    async with point_manager(ses.uid, db) as pm:
+        if pm.magic_points < 10:
+            raise NoPointError(ses.uid)
+
         url = conf.infer.base + conf.infer.image_to_video
         code, resp = await forward_to_infer_srv(url, req)
+
         if code == 0:
-            subscription.remains -= 10
-            subscription.utime = datetime.now()
+            pm.deduct(10)
             logger.info(f"reduce user {ses.nickname} magic points.")
 
         return resp
@@ -187,12 +115,15 @@ async def segment_any(
     conf: deps.Config,
 ) -> Response:
 
-    async with query_valid_subscription(ses.uid, db) as subscription:
+    async with point_manager(ses.uid, db) as pm:
+        if pm.magic_points < 1:
+            raise NoPointError(ses.uid)
+
         url = conf.infer.base + conf.infer.segment_any
         code, resp = await forward_to_infer_srv(url, req)
+
         if code == 0:
-            subscription.remains -= 10
-            subscription.utime = datetime.now()
+            pm.deduct(1)
             logger.info(f"reduce user {ses.nickname} magic points.")
 
         return resp
