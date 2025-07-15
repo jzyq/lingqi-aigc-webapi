3b45bc59446071f804583372b9df3d68947aecb3
Zeng Yu <zeng.yu.1993@gmail.com>
2025-07-15 19:16:22 +0800

    upd: add zhipuai translate client.

---
+++ aigc/prompt_translate.py
@@ -0,0 +1,15 @@
+from zhipuai import ZhipuAI  # type: ignore
+
+
+class ZhipuaiClient:
+
+    def __init__(self, api_key: str) -> None:
+        self._client = ZhipuAI(api_key=api_key)
+
+    def translate(self, text: str) -> str:
+        response = self._client.chat.completions.create(
+            model="glm-4-air-250414", messages=[
+                {"role": "user", "content": f"请将下列内容翻译成英语：{text}"}
+            ]
+        )
+        return response.choices[0].message.content # type: ignore
