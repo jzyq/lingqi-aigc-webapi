04154ef08a969d802844580e6967e9789e9cf806
Zeng Yu <zeng.yu.1993@gmail.com>
2025-09-15 11:03:39 +0800

    upd: temporary export server

--- web/wxproxy/__main__.py
+++ web/wxproxy/__main__.py
@@ -7,7 +7,7 @@ import asyncio
 async def main() -> None:
     await dataio.init("mongodb://localhost:27017/")
 
-    conf = uvicorn.Config(app=make_app())
+    conf = uvicorn.Config(app=make_app(), host="0.0.0.0")
     server = uvicorn.Server(conf)
     await server.serve()
 
