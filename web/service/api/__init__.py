29400c53ccc163026bfac5f8b52d57b259e9af8f
Zeng Yu <zeng.yu.1993@gmail.com>
2025-09-15 11:52:29 +0800

    remove dependice

--- web/service/api/__init__.py
+++ web/service/api/__init__.py
@@ -1,14 +1,17 @@
-from . import wx, user, payment, infer, gallery, main, download, heaven_album
-from . import dev  # type: ignore
-from fastapi import APIRouter
+# from . import wx, user, payment, infer, gallery, main, download, heaven_album
 
-router = APIRouter(prefix="/api")
+from . import wx
 
-router.include_router(wx.router)
-router.include_router(user.router)
-router.include_router(payment.router)
-router.include_router(infer.router)
-router.include_router(gallery.router)
-router.include_router(main.router)
-router.include_router(download.router)
-router.include_router(heaven_album.router)
+# from . import dev  # type: ignore
+# from fastapi import APIRouter
+
+# router = APIRouter(prefix="/api")
+
+# router.include_router(wx.router)
+# router.include_router(user.router)
+# router.include_router(payment.router)
+# router.include_router(infer.router)
+# router.include_router(gallery.router)
+# router.include_router(main.router)
+# router.include_router(download.router)
+# router.include_router(heaven_album.router)
