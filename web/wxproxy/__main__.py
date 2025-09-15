1783297ba9a277027775def965b76d6f768028ef
Zeng Yu <zeng.yu.1993@gmail.com>
2025-09-15 11:25:05 +0800

    init login id

--- web/wxproxy/__main__.py
+++ web/wxproxy/__main__.py
@@ -1,3 +1,4 @@
+import dataio.config
 from . import make_app
 import uvicorn
 import dataio
@@ -6,6 +7,9 @@ import asyncio
 
 async def main() -> None:
     await dataio.init("mongodb://localhost:27017/")
+
+    login_conf = dataio.config.wechat.Login(appid="wx31a857d3d6e9c63e")
+    await login_conf.save()
 
     conf = uvicorn.Config(app=make_app(), host="0.0.0.0")
     server = uvicorn.Server(conf)
